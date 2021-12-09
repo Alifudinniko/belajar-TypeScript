c32cc584e4add5cbc6bad52f4cf4e797e992069b
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-12-09 20:42:12 +0700

    class awal

--- bicara-typescript-master/src/6-class.ts
+++ bicara-typescript-master/src/6-class.ts
@@ -9,3 +9,30 @@
 
 // class --> OOP ( Object Oriented Programming)
 // class --> Object Instance
+
+class User {
+    //Properties
+    id:number;
+    firstName:string;
+    lastName:string;
+
+
+    //Method
+    login(){}
+    register(){}
+
+    //constructor
+    constructor(id:number, firstName:string, lastName:string ){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+}
+
+
+let myUser = new User(1,'alif','niko');
+// myUser.firstName;
+console.log(myUser);
+
+
