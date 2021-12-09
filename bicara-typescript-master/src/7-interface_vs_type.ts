fafbd015cd0debbb83e0cad6695a5239ffcfb9f5
Alifudin Niko Priambodo <m.rozaki09903@gmail.com>
2021-12-09 21:10:56 +0700

    Object and merge

--- bicara-typescript-master/src/7-interface_vs_type.ts
+++ bicara-typescript-master/src/7-interface_vs_type.ts
@@ -6,9 +6,34 @@ export {}; // quick fix for global variable
  * Object
  */
 
+interface User {
+    name:string;
+    age:number;
+}
+
+type TUser = {
+    name:string;
+    age:number;
+}
+
+
 /**
  * Merge
  */
+interface Song{
+    // number:number;
+    songname:string;
+}
+interface Song{
+    artisname:string;
+}
+
+ const mySong :Song= {
+     artisname:'Metal',
+     songname:'niko'
+ }
+
+
 
 /**
  * Intersection & Union
